efec4b78444ba331e20e71edb628a4c09e8ebf57
ferreiria <ferreira.s.joao@gmail.com>
2017-04-02 14:36:35 +0200

    added projects

---
+++ EFCore/SchoolContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DbModels.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore
+{
+    
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        private string databasePath;
+
+        public SchoolContext()
+        {
+
+        }
+
+
+        public SchoolContext(string connString)
+        {
+            databasePath = connString;
+            //Database.SetInitializer<SchoolContext>(null);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer($"Filename={databasePath}");
+        }
+    }
+}
+
---
+++ EFCore/SchoolContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace EFCore
+{
+    public class SchoolContextFactory : IDbContextFactory<SchoolContext>
+    {
+
+        public SchoolContext Create(DbContextFactoryOptions options)
+        {
+            return new EFCore.SchoolContext("Server=(localdb)\\mssqllocaldb;Database=EF6MVCCore;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
